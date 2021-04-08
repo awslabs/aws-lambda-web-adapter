83516dba3ad3058213e9e50868817e9fde255dce
Harold Sun <sunhua@amazon.com>
2021-04-09 01:45:06 +0800

    check runtime environment and support the same image to be run on both lambda and eks.

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     env_logger::init();
-    let app_host: &str = "http://127.0.0.1:8080/";
-    let health_check_path: &str = "healthz";
+    let app_host: &str = "http://127.0.0.1:8080";
+    let health_check_path: &str = "/healthz";
 
     // parse arguments
     let args: Vec<String> = env::args().collect();
@@ -28,7 +28,7 @@ async fn main() -> Result<(), Error> {
     let command_args = &args[2..];
 
     // start user application
-    Command::new(command)
+    let mut app_process = Command::new(command)
         .args(command_args)
         .spawn()
         .expect("failed to start user application");
@@ -39,17 +39,28 @@ async fn main() -> Result<(), Error> {
     })
     .await?;
 
-    // start lambda runtime
-    lambda_runtime::run(handler(http_proxy_handler)).await?;
-    Ok(())
+    // check runtime environment
+    match env::var("AWS_LAMBDA_RUNTIME_API") {
+        // in lambda runtime, start lambda runtime
+        Ok(_val) => {
+            lambda_runtime::run(handler(http_proxy_handler)).await?;
+            Ok(())
+        }
+        // not in lambda, just wait for app process
+        Err(_e) => {
+            app_process.wait().unwrap();
+            Ok(())
+        }
+    }
 }
 
 async fn http_proxy_handler(event: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    let app_host: &str = "http://127.0.0.1:8080/";
-
+    let app_host: &str = "http://127.0.0.1:8080";
     let (parts, body) = event.into_parts();
+    let app_uri = app_host.to_string() + parts.uri.path_and_query().unwrap().as_str();
+    debug!("app_uri: {}", app_uri);
     let res = Client::new()
-        .request(parts.method, app_host)
+        .request(parts.method, app_uri)
         .headers(parts.headers)
         .body(body.to_vec())
         .send()
@@ -58,6 +69,5 @@ async fn http_proxy_handler(event: Request, _: Context) -> Result<impl IntoRespo
     Ok(Response::builder()
         .status(res.status())
         .body(res.text().await?)
-        .expect("failed to send response")
-    )
+        .expect("failed to send response"))
 }
