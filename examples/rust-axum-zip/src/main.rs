b4c01ff5c6e3caac305504292d75ac456809b903
Harold Sun <sunhua@amazon.com>
2022-07-29 12:49:51 +0800

    Add Rust Axum Example (#56)
    * add rust axum example
    
    This example uses custom runtime (provided.al2)

---
+++ examples/rust-axum-zip/src/main.rs
@@ -0,0 +1,11 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
