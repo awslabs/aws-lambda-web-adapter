f42d7a6a0689bd5f0dbd1fd7770b95470fdb3fdd
Harold Sun <sunhua@amazon.com>
2022-09-12 05:15:12 +0800

    configure log level with RUST_LOG environment variable (#75)

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 use lambda_web_adapter::{Adapter, AdapterOptions};
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .with_target(false)
-        .without_time()
-        .init();
+    let filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+    tracing_subscriber::fmt().with_env_filter(filter).without_time().init();
 
     let options = AdapterOptions::from_env();
     lambda_web_adapter::register_default_extension();
