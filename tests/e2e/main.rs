4e00922767623e66d140a2dee7b7bfed46f1929f
Harold Sun <sunhua@amazon.com>
2023-01-17 19:25:26 +0800

    Add a test for oci rest basic request

--- tests/e2e/main.rs
+++ tests/e2e/main.rs
@@ -1,1 +1,15 @@
+use hyper::Client;
+use hyper_tls::HttpsConnector;
+use std::env;
 
+#[ignore]
+#[tokio::test]
+async fn test_oci_rest_basic_request() {
+    let oci_rest_endpoint = env::var("OCI_REST_ENDPOINT").unwrap();
+
+    let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
+
+    let response = client.get((&oci_rest_endpoint).parse().unwrap()).await.unwrap();
+
+    assert_eq!(200, response.status());
+}
