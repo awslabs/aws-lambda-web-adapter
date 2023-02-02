43d966791ff06482487c79ff1e6b9e03b68b26b9
Elon Niu <elonniu@amazon.com>
2023-02-03 06:44:09 +0800

    Removed `aws ecr-public get-login-password`

--- examples/php-zip-snapstart/app/src/main/resources/run.sh
+++ examples/php-zip-snapstart/app/src/main/resources/run.sh
@@ -4,14 +4,6 @@ if [ ! -d '/tmp/session' ]; then
   mkdir -p /tmp/session
 fi
 
-# list runtime libs
-dirs=$(echo "$LD_LIBRARY_PATH" | tr ":" "\n")
-opt_lib=/opt/lib
-dirs=${dirs[@]/$opt_lib/}
-for dir in $dirs; do
-  ls $dir/*so*
-done
-
 /opt/php/bin/php-fpm --force-stderr --fpm-config /var/task/php/etc/php-fpm.conf
 
 exec /opt/nginx/bin/nginx -c /var/task/nginx/conf/nginx.conf -g "daemon off;"
