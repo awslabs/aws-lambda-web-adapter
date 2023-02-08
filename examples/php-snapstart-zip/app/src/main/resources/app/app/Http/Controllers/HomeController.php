684230790d9253182f5e7b5647dcae9d862a4a5b
Elon Niu <elonniu@amazon.com>
2023-02-09 04:02:17 +0800

    Removed `aws ecr-public get-login-password`

--- examples/php-snapstart-zip/app/src/main/resources/app/app/Http/Controllers/HomeController.php
+++ examples/php-snapstart-zip/app/src/main/resources/app/app/Http/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Aws\DynamoDb\DynamoDbClient;
 use Aws\Result;
-use Ramsey\Uuid\Uuid;
 
 class HomeController extends Controller
 {
@@ -16,31 +15,64 @@ class HomeController extends Controller
 
     public function phpinfo()
     {
+        $time_epoch_app        = $this->ms(LARAVEL_START);
+        $time_epoch_controller = $this->ms();
+
+        $_ENV['MEMORY_GET_USAGE'] = memory_get_usage();
+
         if (!isset($_ENV['HTTP_X_AMZN_REQUEST_CONTEXT'])) {
             return phpinfo();
         }
 
-        $this->handle();
-
         $request_context = json_decode($_ENV['HTTP_X_AMZN_REQUEST_CONTEXT'], true);
 
 
-        $requestId            = $request_context['requestId'];
-        $time                 = $request_context['time'];
-        $time_api_gw          = $request_context['timeEpoch'];
-        $time_lambda          = $this->ms($_ENV['REQUEST_TIME_FLOAT']);
-        $time_lambda_instance = $this->ms();
+        $requestId = $request_context['requestId'];
+        $time      = $request_context['time'];
+
+        $time_epoch_api_gw = $request_context['timeEpoch'];
+        $time_epoch_lambda = $this->ms($_ENV['REQUEST_TIME_FLOAT']);
+
 
         $_ENV['REQUESTID'] = $requestId;
         $_ENV['TIME']      = $time;
 
-        $_ENV['TIME_API_GW']          = $time_api_gw;
-        $_ENV['TIME_API_GW_COST']     = $time_lambda - $time_api_gw;
-        $_ENV['TIME_LAMBDA']          = $time_lambda;
-        $_ENV['TIME_LAMBDA_COST']     = $time_lambda_instance - $time_lambda;
-        $_ENV['TIME_LAMBDA_INSTANCE'] = $time_lambda_instance;
+        $_ENV['TIME_EPOCH_API_GW']      = $time_epoch_api_gw;
+        $_ENV['TIME_API_GW_DURATION']   = $time_epoch_lambda - $time_epoch_api_gw;
+        $_ENV['TIME_EPOCH_LAMBDA']      = $time_epoch_lambda;
+        $_ENV['TIME_LAMBDA_DURATION']   = $time_epoch_app - $time_epoch_lambda;
+        $_ENV['TIME_EPOCH_APP']         = $time_epoch_app;
+        $_ENV['TIME_APP_INIT_DURATION'] = $time_epoch_controller - $time_epoch_app;
+        $_ENV['TIME_EPOCH_CONTROLLER']  = $time_epoch_controller;
 
-        $_ENV['COST_FROM_REQUEST'] = $time_lambda_instance - $time_api_gw;
+        $_ENV['DURATION_FROM_API_GW'] = $time_epoch_controller - $time_epoch_api_gw;
+
+        $events = [];
+
+        $events[] = [
+            'PutRequest' => [
+                'Item' => [
+                    'id' => ['S' => $_ENV['REQUESTID']],
+                    'time' => ['S' => $_ENV['TIME']],
+
+                    'time_epoch_api_gw' => ['S' => $_ENV['TIME_EPOCH_API_GW']],
+                    'time_api_gw_duration' => ['S' => $_ENV['TIME_API_GW_DURATION']],
+                    'time_epoch_lambda' => ['S' => $_ENV['TIME_EPOCH_LAMBDA']],
+                    'time_lambda_duration' => ['S' => $_ENV['TIME_LAMBDA_DURATION']],
+                    'time_epoch_app' => ['S' => $_ENV['TIME_EPOCH_APP']],
+                    'time_app_init_duration' => ['S' => $_ENV['TIME_APP_INIT_DURATION']],
+                    'time_epoch_controller' => ['S' => $_ENV['TIME_EPOCH_CONTROLLER']],
+                    'duration_from_api_gw' => ['S' => $_ENV['DURATION_FROM_API_GW']],
+
+                    'aws_execution_env' => ['S' => $_ENV['AWS_EXECUTION_ENV']],
+                    'memory_get_usage' => ['S' => $_ENV['MEMORY_GET_USAGE']],
+                    'version' => ['S' => PHP_VERSION],
+                    'env' => ['S' => json_encode($_ENV)],
+                ],
+            ],
+        ];
+
+        $this->batchWriteItem('us-west-2', 'prod-lambda-runtimes-tests-cost', $events);
 
         return phpinfo();
     }
@@ -76,54 +108,6 @@ class HomeController extends Controller
                 $table => $data,
             ],
         ]);
-    }
-
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle(): int
-    {
-
-        $events = [];
-
-        $count = 0;
-
-        $region = 'us-west-2';
-        $table  = 'prod-lambda-runtimes-tests-cost';
-
-        for ($i = 0; $i < 1; $i++) {
-            $count++;
-            $item = [
-                'PutRequest' => [
-                    'Item' => [
-                        'id' => ['S' => Uuid::uuid4()],
-                        'request_at_ms' => ['S' => $_GET['request_at_ms'] ?? $this->ms()],
-                        'php_start_at' => ['S' => $this->ms($_ENV['REQUEST_TIME_FLOAT'])],
-                        'php_started_at' => ['S' => $this->ms()],
-                        'aws_execution_env' => ['S' => $_ENV['AWS_EXECUTION_ENV']],
-                        'version' => ['S' => PHP_VERSION],
-                        'env' => ['S' => json_encode($_ENV)],
-                    ],
-                ],
-            ];
-
-            $events[] = $item;
-
-            if (count($events) === 10) {
-                $this->batchWriteItem($region, $table, $events);
-                $events = [];
-            }
-        }
-
-        if (count($events)) {
-            $this->batchWriteItem($region, $table, $events);
-        }
-
-//			dump("$count items was written in $region.");
-
-        return 0;
     }
 
 }
