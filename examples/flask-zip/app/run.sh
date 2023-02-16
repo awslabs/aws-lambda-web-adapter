e8a6e309e065554c08db40a60b6b669671bbf439
Harold Sun <sunhua@amazon.com>
2023-02-16 10:52:00 +0000

    upgrade Flask

--- examples/flask-zip/app/run.sh
+++ examples/flask-zip/app/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PATH=$PATH:$LAMBDA_TASK_ROOT/bin PYTHONPATH=$LAMBDA_TASK_ROOT exec gunicorn -b=:$PORT -w=1 app:app
+PATH=$PATH:$LAMBDA_TASK_ROOT/bin PYTHONPATH=$LAMBDA_TASK_ROOT exec python -m gunicorn -b=:$PORT -w=1 app:app
