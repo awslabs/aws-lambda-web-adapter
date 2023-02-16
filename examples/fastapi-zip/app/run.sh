86ac335ed7fac66dd2bc994d7b12214c8fb403be
Harold Sun <sunhua@amazon.com>
2023-02-16 18:58:19 +0800

    upgrade Flask (#158)

--- examples/fastapi-zip/app/run.sh
+++ examples/fastapi-zip/app/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PATH=$PATH:$LAMBDA_TASK_ROOT/bin PYTHONPATH=$LAMBDA_TASK_ROOT exec uvicorn --port=$PORT main:app
+PATH=$PATH:$LAMBDA_TASK_ROOT/bin PYTHONPATH=$LAMBDA_TASK_ROOT exec python -m uvicorn --port=$PORT main:app
