fe5a66ff985b48959260ddf1f9ad306b0fb70caf
harold <harold@pwrdrvr.com>
2023-02-16 09:30:49 -0500

    Add E2Es for compressed responses

--- tests/e2e/main.rs
+++ tests/e2e/main.rs
@@ -1,9 +1,12 @@
+use flate2::read::GzDecoder;
 use http::Uri;
 use hyper::{Body, Client, Method, Request};
 use hyper_tls::HttpsConnector;
 use lambda_http::aws_lambda_events::serde_json;
 use lambda_http::aws_lambda_events::serde_json::Value;
 use std::env;
+use std::io;
+use std::io::prelude::*;
 
 fn get_endpoints() -> Vec<Option<String>> {
     let configurations = [
@@ -18,6 +21,13 @@ fn get_endpoints() -> Vec<Option<String>> {
     ];
 
     configurations.iter().map(|e| env::var(e).ok()).collect()
+}
+
+fn decode_reader(bytes: &Vec<u8>) -> io::Result<String> {
+    let mut gz = GzDecoder::new(&bytes[..]);
+    let mut s = String::new();
+    gz.read_to_string(&mut s)?;
+    Ok(s)
 }
 
 #[ignore]
@@ -85,6 +95,36 @@ async fn test_http_query_params() {
             assert!(body["args"]["fizz"][0].is_string());
             assert_eq!(Some("buzz"), body["args"]["fizz"][0].as_str());
             assert_eq!(Some("bar"), body["args"]["foo"][0].as_str());
+        }
+    }
+}
+
+#[ignore]
+#[tokio::test]
+async fn test_http_compress() {
+    for endpoint in get_endpoints().iter() {
+        if let Some(endpoint) = endpoint {
+            let client = Client::builder().build::<_, hyper::Body>(HttpsConnector::new());
+            let parts = endpoint.parse::<Uri>().unwrap().into_parts();
+            let uri = Uri::builder()
+                .scheme(parts.scheme.unwrap())
+                .authority(parts.authority.unwrap())
+                .path_and_query("/html")
+                .build()
+                .unwrap();
+            let req = Request::builder()
+                .method(Method::GET)
+                .header("accept-encoding", "gzip")
+                .uri(uri)
+                .body(Body::empty())
+                .unwrap();
+            let resp = client.request(req).await.unwrap();
+            let (parts, body) = resp.into_parts();
+            let body_bytes = hyper::body::to_bytes(body).await.unwrap();
+            let body = decode_reader(&body_bytes.to_vec()).unwrap();
+
+            assert_eq!(200, parts.status.as_u16());
+            assert!(body.contains("<html>"));
         }
     }
 }
