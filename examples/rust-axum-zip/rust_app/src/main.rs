15ba09d77dfe918ad498f712213d24a967ab08b9
Harold Sun <sunhua@amazon.com>
2023-02-26 07:38:41 +0000

    Update Axum example to use new SAM CLI rust build

--- examples/rust-axum-zip/rust_app/src/main.rs
+++ examples/rust-axum-zip/rust_app/src/main.rs
@@ -2,7 +2,7 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, world!!!" }));
 
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
