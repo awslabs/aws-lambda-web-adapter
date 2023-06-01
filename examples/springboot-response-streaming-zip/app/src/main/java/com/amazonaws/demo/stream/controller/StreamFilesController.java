83b42c3f15b236a0b2a0d1305be435f9b84ed16a
Muhammed Soyer <msoyer@gmail.com>
2023-05-31 20:53:04 -0700

    Optimized imports

--- examples/springboot-response-streaming-zip/app/src/main/java/com/amazonaws/demo/stream/controller/StreamFilesController.java
+++ examples/springboot-response-streaming-zip/app/src/main/java/com/amazonaws/demo/stream/controller/StreamFilesController.java
@@ -1,17 +1,16 @@
 package com.amazonaws.demo.stream.controller;
 
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.*;
-import java.nio.file.Files;
 
 @RestController
 @EnableWebMvc
