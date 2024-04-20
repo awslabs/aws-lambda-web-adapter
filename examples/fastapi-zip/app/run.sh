7abe0852370ced427e64da8cd91f9739b9b24784
Souyama <souyamadebnath@gmail.com>
2024-04-20 21:40:50 +0530

    Example Fix: python dependencies from layers (#447)

--- examples/fastapi-zip/app/run.sh
+++ examples/fastapi-zip/app/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-PATH=$PATH:$LAMBDA_TASK_ROOT/bin PYTHONPATH=$LAMBDA_TASK_ROOT exec python -m uvicorn --port=$PORT main:app
+PATH=$PATH:$LAMBDA_TASK_ROOT/bin \
+    PYTHONPATH=$LAMBDA_TASK_ROOT:$PYTHONPATH:/opt/python \
+    exec python -m uvicorn --port=$PORT main:app
--- examples/flask-zip/app/run.sh
+++ examples/flask-zip/app/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-PATH=$PATH:$LAMBDA_TASK_ROOT/bin PYTHONPATH=$LAMBDA_TASK_ROOT exec python -m gunicorn -b=:$PORT -w=1 app:app
+PATH=$PATH:$LAMBDA_TASK_ROOT/bin \
+    PYTHONPATH=$LAMBDA_TASK_ROOT:$PYTHONPATH:/opt/python \
+    exec python -m gunicorn -b=:$PORT -w=1 app:app
