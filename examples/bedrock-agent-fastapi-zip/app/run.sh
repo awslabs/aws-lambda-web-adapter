7709bb4cf7f215a087fbeeac7b8f611a868352e7
renjiexu-amzn <168146879+renjiexu-amzn@users.noreply.github.com>
2024-04-27 04:12:01 -0700

    [#453] Update the Fast API run script examples to include LAMBDA_RUNTIME_DIR in search path. (#454)

--- examples/bedrock-agent-fastapi-zip/app/run.sh
+++ examples/bedrock-agent-fastapi-zip/app/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PATH=$PATH:$LAMBDA_TASK_ROOT/bin:/opt/bin PYTHONPATH=$LAMBDA_TASK_ROOT:/opt/python exec python -m uvicorn --port=$PORT main:app
+PATH=$PATH:$LAMBDA_TASK_ROOT/bin:/opt/bin PYTHONPATH=/opt/python:$LAMBDA_RUNTIME_DIR exec python -m uvicorn --port=$PORT main:app
