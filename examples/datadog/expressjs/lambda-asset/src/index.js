5f73f45ee08d331975d86cabac637272c674c769
alexgallotta <5581237+alexgallotta@users.noreply.github.com>
2025-05-02 13:43:11 -0400

    add examples for datadog

---
+++ examples/datadog/expressjs/lambda-asset/src/index.js
@@ -0,0 +1,27 @@
+const tracer = require("dd-trace").init();
+tracer.use("http", {
+  blocklist: ["/"],
+});
+const express = require("express");
+const app = express();
+const port = process.env["PORT"] || 8080;
+
+// SIGTERM Handler
+process.on("SIGTERM", async () => {
+  console.info("[express] SIGTERM received");
+
+  console.info("[express] cleaning up");
+  // perform actual clean up work here.
+  await new Promise((resolve) => setTimeout(resolve, 100));
+
+  console.info("[express] exiting");
+  process.exit(0);
+});
+
+app.get("/call_lwa", (_req, res) => {
+  res.send("Hi there!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
