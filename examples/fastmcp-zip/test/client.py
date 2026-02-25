fb77dae6b667d966fcc643f5eff697fd608fe7dc
Steven Cook <awscook@amazon.com>
2026-02-25 03:55:24 +0000

    example: Fixed docker, added README

--- examples/fastmcp-zip/test/client.py
+++ examples/fastmcp-zip/test/client.py
@@ -1,11 +1,11 @@
 import asyncio
 from fastmcp import Client
 
-client = Client("https://qeejoj5nk4.execute-api.ap-southeast-2.amazonaws.com/mcp")
+client = Client("https://xxxxxxxx.execute-api.ap-southeast-2.amazonaws.com/mcp")
 
 async def call_tool(name: str):
     async with client:
         result = await client.call_tool("greet", {"name": name})
         print(result)
 
-asyncio.run(call_tool("Ford"))
+asyncio.run(call_tool("Victoria"))
--- examples/fastmcp/test/client.py
+++ examples/fastmcp/test/client.py
@@ -1,11 +1,11 @@
 import asyncio
 from fastmcp import Client
 
-client = Client("https://qeejoj5nk4.execute-api.ap-southeast-2.amazonaws.com/mcp")
+client = Client("https://xxxxxxxxx.execute-api.ap-southeast-2.amazonaws.com/mcp")
 
 async def call_tool(name: str):
     async with client:
         result = await client.call_tool("greet", {"name": name})
         print(result)
 
-asyncio.run(call_tool("Ford"))
+asyncio.run(call_tool("Victoria"))
